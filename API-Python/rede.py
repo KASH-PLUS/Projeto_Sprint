ab9928664dd9bca2012718224e9c9077cac1624e
ViniScardoso <vinicius.cardoso@sptech.school>
2022-11-10 01:19:11 -0300

    informações listadas no python

--- API-Python/rede.py
+++ API-Python/rede.py
@@ -1,11 +1,19 @@
 from psutil import *
 
-print(net_if_stats())
+placa = net_if_addrs()['eno1']
 
-print("-" * 50)                 
+print(placa)
+print("-"*50)
 
-print(net_if_addrs())
+print("Ipv4 Local: " + placa[0].address)
+print("Mascara de rede ipv4: " + placa[0].netmask)
+print("Ipv6 Local: " + placa[1].address[:25])
+print("Mascara de rede ipv6: " + placa[1].netmask) 
+print("MAC Address: " + placa[2].address)
 
-print("-" * 50)
+print("-"*50)
 
 print(net_io_counters())
+
+print("-"*50)
+
