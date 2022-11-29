c3176f3ad41a4c09fff58fed43ca9692815bf958
davibordon <davi.silva@sptech.school>
2022-11-29 15:29:36 -0300

    Adicionando KPIs

--- API-Python/dsa.py
+++
@@ -1,8 +0,0 @@
-import psutil
-
-ociosidade = psutil.cpu_times_percent(percpu=False).idle / 3600
-temperatura = psutil.sensors_temperatures()['nvme'][0].current
-
-print("Ociosidade:", round(ociosidade,2), "Horas")
-
-print("Temperatura:", temperatura, "°C")
